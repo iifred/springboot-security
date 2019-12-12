5ae834edc72724f2c11a6ee4ada665a4103ac76a
iitfred <594703382@qq.com>
2019-12-12 23:09:22 +0800

    1：初始化项目

---
+++ src/main/java/com/springboot/security/config/MySecurityConfig.java
@@ -0,0 +1,18 @@
+package com.springboot.security.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //定制请求的授权规则
+        http.authorizeRequests().antMatchers();
+    }
+}
